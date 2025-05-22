fc7f585498f6a6c09339c557095acb325bcf4774
Heymalhar <145997394+Heymalhar@users.noreply.github.com>
2025-05-22 19:42:35 +0530

    Update engine.py: Add more backend functionalities

--- backend/recommender/engine.py
+++ backend/recommender/engine.py
@@ -28,8 +28,7 @@ def compute_mean_vector(track_names):
     return np.mean(vectors, axis=0)
 
 def recommend_tracks(input_vector, input_track_ids, seen_ids):
-
-    noisy_vector = input_vector + np.random.normal(0, NOISE_STD, size = input_vector.shape)
+    noisy_vector = input_vector + np.random.normal(0, NOISE_STD, size=input_vector.shape)
 
     cursor = tracks_col.find({}, {
         "track_name": 1, "artists": 1, "track_genre": 1,
@@ -49,6 +48,7 @@ def recommend_tracks(input_vector, input_track_ids, seen_ids):
     sampled = random.sample(top_similar, k=min(SAMPLE_K, len(top_similar)))
 
     recommendations = []
+
     for tid in sampled:
         doc = next(item for item in all_tracks if item["track_id"] == tid)
         doc["similarity_score"] = round(similarities[track_ids.index(tid)], 4)
@@ -58,7 +58,6 @@ def recommend_tracks(input_vector, input_track_ids, seen_ids):
     return recommendations
 
 def store_recommendations(username, input_ids, new_recommendations):
-
     combination_key = "_".join(map(str, sorted(input_ids)))
     new_ids = [rec["track_id"] for rec in new_recommendations]
 
@@ -90,32 +89,124 @@ def store_recommendations(username, input_ids, new_recommendations):
 
     return new_recommendations
 
+def user_signup(username, email, password):
+
+    if users_col.find_one({"username": username}):
+        raise ValueError("Username already exists.")
+    
+    users_col.insert_one({
+        "username": username,
+        "email": email,
+        "password": password,
+        "recommendation_history": {}
+    })
+    return f"User '{username}' signed up successfully."
+
+def user_login(username, password):
+    user = users_col.find_one({"username": username})
+    if not user:
+        raise ValueError("User not found.")
+    
+    if user.get("password") != password:
+        raise ValueError("Incorrect Password.")
+    
+    return f"User '{username}' logged in successfully."
+
+def previous_recommendations(username, track_names):
+
+    track_ids = [get_track_vector(name)[1] for name in track_names]
+    combination_key = "_".join(map(str, sorted(track_ids)))
+
+    user = users_col.find_one({"username": username})
+    if not user:
+        raise ValueError("User not found.")
+    
+    recommended_ids = user.get("recommendation_history", {}).get(combination_key, [])
+    if not recommended_ids:
+        return []
+    
+    recommend_tracks = []
+    for tid in recommended_ids:
+        doc = tracks_col.find_one({"track_id": tid}, {
+            "track_id": 1,
+            "track_name": 1,
+            "artists": 1,
+            "track_genre": 1
+        })
+        if doc:
+            recommend_tracks.append(doc)
+
+    return recommend_tracks
+
 if __name__ == "__main__":
-    print("Welcome!")
+    print("!!WELCOME!!")
 
-    username = input("username: ").strip()
-    track_inputs = [input(f"Enter track name {i+1}: ").strip() for i in range(3)]
+    while True:
+        print("\nSelect an option: ")
+        print("1. Sign Up")
+        print("2. Log in")
+        print("3. Get Recommendations")
+        print("4. View Previous Recommendations.")
+        print("5. Exit")
 
-    try:
-        input_vector = compute_mean_vector(track_inputs)
-        input_ids = [get_track_vector(name)[1] for name in track_inputs]
+        choice = input("Enter choice (1-5): ").strip()
 
-        combination_key = "_".join(map(str, sorted(input_ids)))
+        try:
+            
+            if choice == "1":
+                username = input("Enter username: ").strip()
+                email = input("Enter email: ").strip()
+                password = input("Enter password: ").strip()
+                msg = user_signup(username, email, password)
+                print(msg)
 
-        user_doc = users_col.find_one({"username": username})
-        seen_ids = user_doc.get("recommendation_history", {}).get(combination_key, []) if user_doc else []
+            elif choice == "2":
+                username = input("Enter username: ").strip()
+                password = input("Enter password: ").strip()
+                msg = user_login(username, password)
+                print(msg)
 
-        recommendations = recommend_tracks(input_vector, input_ids, seen_ids)
-        final_output = store_recommendations(username, input_ids, recommendations)
+            elif choice == "3":
+                username = input("Enter username: ").strip()
+                track_inputs = [input(f"Enter track name {i+1}: ").strip() for i in range(3)]
 
-        if not final_output:
-            print("\n ALL CAUGHT UP")
-        else:
-            print(f"\nRecommendations for {username}: ")
-            for idx, track in enumerate(final_output, 1):
-                print(f"{idx}. {track['track_name']} by {track['artists']} (Genre: {track['track_genre']})")
+                input_vector = compute_mean_vector(track_inputs)
+                input_ids = [get_track_vector(name)[1] for name in track_inputs]
 
-    except ValueError as ve:
-        print(f"\nError: {ve}")
-    except Exception as e:
-        print(f"\nUnexpected erro: {str(e)}")
+                combination_key = "_".join(map(str, sorted(input_ids)))
+                user_doc = users_col.find_one({"username": username})
+                seen_ids = user_doc.get("recommendation_history", {}).get(combination_key, []) if user_doc else []
+
+                recommendations = recommend_tracks(input_vector, input_ids, seen_ids)
+                final_output = store_recommendations(username, input_ids, recommendations)
+
+                if not final_output:
+                    print("\nAll Caught Up.")
+                else:
+                    print(f"\n Recommendations for {username}:")
+                    for idx, track in enumerate(final_output, 1):
+                        print(f"{idx}. {track['track_name']} by {track['artists']} (Genre: {track['track_genre']})")
+
+            elif choice == "4":
+                username = input("Enter username: ").strip()
+                track_inputs = [input(f"Enter track name {i+1}: ").strip() for i in range(3)]
+                history = previous_recommendations(username, track_inputs)
+
+                if not history:
+                    print("No previous recommendations for .this combination.")
+                else:
+                    print(f"\nPrevious Recommendations for {username}:")
+                    for idx, track in enumerate(history, 1):
+                        print(f"{idx}. {track['track_name']} by {track['artists']} (Genre: {track['track_genre']})")
+
+            elif choice == "5":
+                print("Exiting.")
+                break
+                
+            else:
+                print("Invalid option. Try again.")
+
+        except ValueError as ve:
+            print(f"\nError: {ve}")
+        except Exception as e:
+            print(f"\nUnexpected error: {str(e)}")
